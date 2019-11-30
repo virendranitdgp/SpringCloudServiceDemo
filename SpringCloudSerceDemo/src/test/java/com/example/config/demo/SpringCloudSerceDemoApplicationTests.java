354bfafab3f7bb9f216c656e6e80ed3b0a5c692b
Virendra Kumar <virendra@codecraft.co.in>
2019-11-30 13:12:12 +0530

    First Commit

---
+++ SpringCloudSerceDemo/src/test/java/com/example/config/demo/SpringCloudSerceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.config.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSerceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
